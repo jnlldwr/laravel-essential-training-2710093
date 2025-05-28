f2433ff2dfff5650819a2a4441baec30eedfcdae
jnlldwr <103033473+jnlldwr@users.noreply.github.com>
2025-05-28 10:33:47 +0000

    01 Before Challenge

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-base>
+
+<x-slot name="name">Janelle</x-slot>
+
+<x-slot name="content">
+  <h1 class="text-4xl font-bold">Home</h1>
+  <p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+</x-slot>
+
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
